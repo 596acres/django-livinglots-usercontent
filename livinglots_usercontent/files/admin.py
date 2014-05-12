40639f457a13de93fc59c6160dbd60352801bab7
Eric Brelsford <ebrelsford@gmail.com>
2014-05-12 17:40:04 -0400

    Also not likely to want to edit added

--- livinglots_usercontent/files/admin.py
+++ livinglots_usercontent/files/admin.py
@@ -6,7 +6,7 @@ from .models import File
 class FileAdmin(admin.ModelAdmin):
     list_display = ('title', 'added_by_name', 'added',)
     list_filter = ('added',)
-    readonly_fields = ('content_type', 'object_id',)
+    readonly_fields = ('added', 'content_type', 'object_id',)
     search_fields = ('title', 'description', 'added_by_name',)
 
 
