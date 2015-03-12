d7a8d4a99093c46d1d9660347ee7b11423a3fc9a
Eric Brelsford <ebrelsford@gmail.com>
2015-03-12 11:49:24 -0400

    ContentMixin

--- livinglots_usercontent/files/models.py
+++ livinglots_usercontent/files/models.py
@@ -1,23 +1,13 @@
-from django.contrib.contenttypes import generic
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from ..models import ContentMixin
 
-class File(models.Model):
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
 
-    document = models.FileField(_('document'), upload_to='files')
-
-    title = models.CharField(_('title'), max_length=256, null=True, blank=True)
+class File(ContentMixin, models.Model):
     description = models.TextField(_('description'), null=True, blank=True)
-    added = models.DateTimeField(auto_now_add=True)
-    added_by = models.ForeignKey(User, null=True, blank=True)
-    added_by_name = models.CharField(_('added by name'), max_length=256,
-                                     null=True, blank=True)
+    document = models.FileField(_('document'), upload_to='files')
+    title = models.CharField(_('title'), max_length=256, null=True, blank=True)
 
     def __unicode__(self):
         if self.title:
--- livinglots_usercontent/photos/models.py
+++ livinglots_usercontent/photos/models.py
@@ -1,18 +1,13 @@
-from django.contrib.contenttypes import generic
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from imagekit.models import ImageSpecField
 from imagekit.processors.resize import SmartResize
 
+from ..models import ContentMixin
 
-class Photo(models.Model):
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
 
+class Photo(ContentMixin, models.Model):
     original_image = models.ImageField(_('original image'), upload_to='photos')
     formatted_image = ImageSpecField(
         format='JPEG',
@@ -28,10 +23,6 @@ class Photo(models.Model):
 
     name = models.CharField(_('name'), max_length=256, null=True, blank=True)
     description = models.TextField(_('description'), null=True, blank=True)
-    added = models.DateTimeField(auto_now_add=True)
-    added_by = models.ForeignKey(User, null=True, blank=True)
-    added_by_name = models.CharField(_('added by name'), max_length=256,
-                                     null=True, blank=True)
 
     def __unicode__(self):
         if self.name:
