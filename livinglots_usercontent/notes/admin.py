09ecb4c06186e3cdc9c16eef310a5aa4482da9ba
Eric Brelsford <ebrelsford@gmail.com>
2015-06-02 15:13:08 -0400

    Allow wysiwyg editing for notes' text

--- livinglots_usercontent/notes/admin.py
+++ livinglots_usercontent/notes/admin.py
@@ -8,5 +8,12 @@ class NoteAdmin(BaseUserContentAdmin):
     list_display = ('added_by_name', 'text', 'added', 'linked_target',)
     search_fields = ('text', 'added_by_name',)
 
+    try:
+        import django_wysiwyg
+
+        change_form_template = 'notes/admin/change_form.html'
+    except ImportError:
+        pass
+
 
 admin.site.register(Note, NoteAdmin)
