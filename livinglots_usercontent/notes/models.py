ec14af3a0d817af3f0e9aed55945c6c95b31fc55
Eric Brelsford <ebrelsford@gmail.com>
2016-07-21 12:06:46 -0400

    Unescaped text for notes

--- livinglots_usercontent/notes/models.py
+++ livinglots_usercontent/notes/models.py
@@ -1,3 +1,5 @@
+from HTMLParser import HTMLParser
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -6,6 +8,11 @@ from ..models import ContentMixin
 
 class Note(ContentMixin, models.Model):
     text = models.TextField(_('text'))
+    
+    def _text_unescaped(self):
+        return HTMLParser().unescape(self.text)
+
+    text_unescaped = property(_text_unescaped)
 
     def __unicode__(self):
         return '%d' % self.pk
